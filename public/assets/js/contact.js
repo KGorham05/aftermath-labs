4e39bc8a4b3bf389d1a08313ac6fa9ac4ee05262
Kgor <kevin.gorham@gmail.com>
2019-05-14 11:24:13 -0700

    made spalsh flask smaller

--- public/assets/js/contact.js
+++ public/assets/js/contact.js
@@ -2,5 +2,18 @@ $(document).ready(function () {
 
     $('#logo').addClass('animate-it');
 
-   
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbyQODSq6BlayMNUPo3OVlp9HYEkFQMxtv9YH_LhnTg-n6HrSj43/exec'
+    const form = document.forms['submit-to-google-sheet']
+
+    form.addEventListener('submit', e => {
+        e.preventDefault()
+        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+            .then(response => console.log('Success!', response))
+            .catch(error => console.error('Error!', error.message))
+    })
 });
+
+// logos 30% smaller
+// add tagline to header main screen
+// add parallax background to desktop
+// add media query set background on phone screens
