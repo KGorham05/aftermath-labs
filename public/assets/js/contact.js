60ca450a58e07e17b27817bcecbe613f886b8871
Kgor <kevin.gorham@gmail.com>
2019-05-13 17:34:23 -0700

    added gforms logic

--- public/assets/js/contact.js
+++ public/assets/js/contact.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
 
     $('#logo').addClass('animate-it');
-        
+
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbyQODSq6BlayMNUPo3OVlp9HYEkFQMxtv9YH_LhnTg-n6HrSj43/exec'
+    const form = document.forms['submit-to-google-sheet']
+
+    form.addEventListener('submit', e => {
+        e.preventDefault()
+        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+            .then(response => console.log('Success!', response))
+            .catch(error => console.error('Error!', error.message))
+    })
 });
